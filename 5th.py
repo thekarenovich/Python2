8bf730f6ee0bcc5d934ca3bb8fc3d385b697a56e
Erik <78443570+thekhachik@users.noreply.github.com>
2021-08-04 23:38:11 +0300

    Add files via upload

---
+++ 5th.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+
+def first():
+    
+    def get():
+        s=text_box.get("1.0", tk.END)
+        # в get() обязательно нужно писать хотя бы один индекс
+        # иначе будет ошибка
+        # тут нужно указать в виде строки
+        # 1 - номер строки
+        # 0 - индекс символа строки
+        if s:
+            print(s)
+        else:
+            print('Empty')
+
+    def delete():
+        # s=text_box.delete("1.0", "2.2")
+        ss=text_box.delete("1.0", tk.END)
+
+    def insert():
+        s=text_box.insert("5.4","DUDE")
+
+    window=tk.Tk()
+    window.title("FIRST")
+    window.geometry("500x650") 
+    window.resizable(False, False)
+
+    text_box = tk.Text()
+    text_box.pack()
+
+    #Кнопка получения введённого текста 
+    button_1=tk.Button(window, text="GET",
+                     activebackground='white',
+                     activeforeground='red',
+                     command=get)
+    button_1.pack()
+
+
+    #Кнопка удаления введённого текста 
+    button_2=tk.Button(window, text="DELETE",
+                    activebackground='white',
+                    activeforeground='red',
+                    command=delete)
+    button_2.pack()
+
+
+    #Кнопка вставки текста DUDE
+    button_3=tk.Button(window, text="INSERT DUDE",
+                activebackground='white',
+                activeforeground='red',
+                command=insert)
+    button_3.pack()
+
+    window.mainloop()
+
+if __name__=="__main__":
+    first()
