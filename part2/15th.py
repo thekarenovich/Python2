6f4df7a68a0b37327738805fc15992115b1d010d
Erik <78443570+thekhachik@users.noreply.github.com>
2022-01-08 20:58:06 +0300

    Create 15th.py

---
+++ part2/15th.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+window = Tk()
+window.title("canvas")
+
+c = Canvas(window, width=200, height=200, bg='white')
+c.pack()
+
+c.create_line(10, 10, 190, 50)
+#первые два параметра - координаты конца линии
+#другие два - другого конца
+
+c.create_line(100, 180, 100, 60,
+              fill='green',
+              width=5,
+              arrow=LAST,
+              dash=(10, 5), #первый параметр увеличивает количество полос; второй - толщина расстояния между полос
+              activefill='lightgreen',
+              arrowshape="10 20 10" #вид стрелки
+              )
+
+window.mainloop()
