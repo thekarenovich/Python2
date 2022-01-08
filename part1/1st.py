8f0294ede3bd0b76fc881f15a6e0b59c6cb41aa3
Erik <78443570+thekhachik@users.noreply.github.com>
2022-01-08 20:15:57 +0300

    Update 1st.py

--- part1/1st.py
+++ part1/1st.py
@@ -6,6 +6,7 @@ def first():
     window.title("FIRST")
     window.geometry("500x300+400+250") 
     window.resizable(False, False)
+    window.wm_attributes("-topmost", 1) #поверх других окон
     #window.minsize(300, 150)
     #window.maxsize(500, 300)
 
