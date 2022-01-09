458e66d1fc5c2e5746f0316425380935bdab0902
Erik <78443570+thekhachik@users.noreply.github.com>
2022-01-09 09:51:16 +0300

    Update 20th.py

--- part2/20th.py
+++ part2/20th.py
@@ -15,3 +15,22 @@ def focus(event):
 c.bind('<FocusIn>', focus)
 
 window.mainloop()
+
+
+'''
+# Можно совместно менять качества объектов через tag:
+
+def color(event):
+     c.itemconfig('group1', width=3,
+                  fill="red")
+ 
+ 
+root = Tk()
+c = Canvas(width=460, height=150, 
+           bg='white')
+c.pack()
+oval = c.create_oval(30, 10, 130, 80, 
+                     tag="group1")
+c.create_line(10, 100, 450, 100, 
+              tag="group1")
+c.bind('<Button-3>', color)'''
